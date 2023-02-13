5dd86961c2ae31d5a5cd58fb72778db29c3e1ef1
LastBeat <wonn5238@gmail.com>
2023-02-13 23:41:44 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
         System.out.println("a is " + a);
         System.out.println("a+1 is " + (a+1));
-        System.out.println("'a+2' is " + (a+2));
+        System.out.println("a+2 is " + (a+2));
+        System.out.println("a+2 is " + (a+3));
     }
 }
