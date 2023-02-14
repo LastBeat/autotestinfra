9f4e6b13c4f51c2d749230d60041c9aab9b2f2b3
LastBeat <125044189+LastBeat@users.noreply.github.com>
2023-02-14 22:18:08 +0900

    Update Main.java
    Refactor Main.java with reference to pr-sonar-bot

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
+    private final static Logger LOG = Logger.getGlobal();
     public static void main(String[] args) {
         int a = 0;
 
-        for(int i = a; i < 100; i++){
+        for(int i = a; i < 10; i++){
             if(i == 0)
-                System.out.println(i + " is default a value");
+                // As-is
+                // System.out.println(i + " is default a value");
+
+                // To-Be
+                LOG.info(i + " is default a value");
+
 
             else
-                System.out.println("a+" + i + " is " + (a+i));
+                // As-is
+                // System.out.println("a+" + i + " is " + (a+i));
+
+                // To-Be
+                LOG.info("a+" + i + " is " + (a+i));
         }
+
+        LOG.info("END.");
+
     }
 }
